75a97c0cfbb234db79367a60424fdc5905b5d5cf
James Requa <james@carteblanche.com>
2017-04-27 23:27:41 -0700

    SelectorDIC
    -Implemented DIC
    - select best model based on Discriminative Information Criterion

--- my_model_selectors.py
+++ my_model_selectors.py
@@ -103,3 +103,59 @@ class SelectorBIC(ModelSelector):
                 bic = float('-inf')
 
         return self.base_model(best_n_state)
+
+
+
+class SelectorDIC(ModelSelector):
+    ''' select best model based on Discriminative Information Criterion
+
+    Biem, Alain. "A model selection criterion for classification: Application to hmm topology optimization."
+    Document Analysis and Recognition, 2003. Proceedings. Seventh International Conference on. IEEE, 2003.
+    http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.58.6208&rep=rep1&type=pdf
+    DIC = log(P(X(i)) - 1/(M-1)SUM(log(P(X(all but i))
+    '''
+
+
+
+    def select(self):
+        warnings.filterwarnings("ignore", category=DeprecationWarning)
+        warnings.filterwarnings("ignore", message="divide by zero encountered in log")
+
+        best_dic = None
+        best_n_state = self.n_constant
+
+        # Create a list of all words except the current word
+        other_words = list(self.words)
+        other_words.remove(self.this_word)
+
+        for n_state in range(self.min_n_components, self.max_n_components+1):
+            try:
+                # Fits model for this word
+                hmm_model = self.base_model(n_state)
+                # Gets score for this word
+                logL_thisword = hmm_model.score(self.X, self.lengths)
+
+                sum_other_scores = 0.0
+
+                # Check the scores of all other words so we can compare
+                for word in other_words:
+                    # X, lengths corresponding to that other word
+                    X, lengths = self.hwords[word]
+
+                    # Total up the scores for all the other words
+                    sum_other_scores += hmm_model.score(X, lengths)
+
+                    # calculate the total number of other words, need to subtract one since this_word not included in average
+                    m = len(self.words) - 1
+
+                # Calculate DIC Score as DIC = log(P(thisword)) - average(log(P(otherwords)))
+                dic =  logL_thisword - (sum_other_scores / m)
+
+                # Keep the highest DIC score
+                if best_dic is None or best_dic < dic:
+                    best_dic = dic
+                    best_n_state = n_state
+            except:
+                    pass
+
+        return self.base_model(best_n_state)
